123ae068e7d849c38076c922185fdde44afacf95
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2017-08-20 14:33:15 +0900

    listingtable filter update
    - new class ListingTable
      - holds a counter
      - copy/reuse fenced_action() as listingtable()
      - every call of listingtable() increases counter by 1
      - the counter is used to generate label name; this avoids label name
    conflict

--- include.py
+++ include.py
@@ -59,70 +59,77 @@ alignment=['AlignDefault'], width=[1], rows=1, cols=1)
 """
 
 
-def fenced_action(options, data, element, doc):
-    # We'll only run this for CodeBlock elements of class 'listingtable'
+class ListingTable(object):
 
-    # pf.debug(doc.get_metadata('include', 'no hoge'))
-    # pf.debug(element.parent)
-    # pf.debug(element.index)
-    # pf.debug(element.parent.content)
+    def __init__(self):
+        self.counter = 0
 
-    # pf.debug(element.parent)
-    # pf.debug(element.prev)
-    # pf.debug(element)
-    # pf.debug(element.next)
+    def listingtable(self, options, data, element, doc):
+        self.counter += 1
+        # We'll only run this for CodeBlock elements of class 'listingtable'
 
-    fn = options.get('source')
-    basename = os.path.basename(fn)
-    isTex = options.get('tex', False)
-    isDocx = options.get('isDocx', False)
-    file_type = options.get('class', 'plain')
-    types = [file_type, 'numberLines']
-    # pf.debug(file_type, types)
-    for doctype in [isTex, isDocx]:
-        if not isinstance(doctype, bool):
-            if isinstance(doctype, str):
-                doctype = doctype.upper()
-                if doctype in ['TRUE', 'YES']:
-                    doctype = True
+        # pf.debug(doc.get_metadata('include', 'no hoge'))
+        # pf.debug(element.parent)
+        # pf.debug(element.index)
+        # pf.debug(element.parent.content)
+
+        # pf.debug(element.parent)
+        # pf.debug(element.prev)
+        # pf.debug(element)
+        # pf.debug(element.next)
+
+        fn = options.get('source')
+        basename = os.path.basename(fn)
+        isTex = options.get('tex', False)
+        isDocx = options.get('isDocx', False)
+        file_type = options.get('class', 'plain')
+        types = [file_type, 'numberLines']
+        # pf.debug(file_type, types)
+        for doctype in [isTex, isDocx]:
+            if not isinstance(doctype, bool):
+                if isinstance(doctype, str):
+                    doctype = doctype.upper()
+                    if doctype in ['TRUE', 'YES']:
+                        doctype = True
+                    else:
+                        doctype = False
                 else:
                     doctype = False
-            else:
-                doctype = False
 
-    if isTex and isDocx:
-        pass
-    else:
-        with codecs.open(fn, 'r', 'utf-8') as f:
-            # pf.debug("codecs.open()")
-            raw = f.read()
-        # pf.debug(raw)
+        if isTex and isDocx:
+            pass
+        else:
+            with codecs.open(fn, 'r', 'utf-8') as f:
+                # pf.debug("codecs.open()")
+                raw = f.read()
+            # pf.debug(raw)
 
-        label = basename.lower().replace(".", "_").replace("/", "_")
-        anchor = "" if not isDocx else 'TC "[@lst:%s] %s" `\l` 6\n\n' % (label, basename)
-        file_title = basename if not isTex else basename.replace("_", "\\\\\\_")
-        # pf.debug(label, anchor, file_title)
+            label = basename.lower().replace(".", "_").replace("/", "_") + str(self.counter)
+            anchor = "" if not isDocx else 'TC "[@lst:%s] %s" `\l` 6\n\n' % (label, basename)
+            file_title = basename if not isTex else basename.replace("_", "\\\\\\_")
+            # pf.debug(label, anchor, file_title)
 
-        # header_caption = pf.Str("Listing: %s %s" % (file_title, anchor))
-        header_block = pf.CodeBlock("", identifier="lst:%s" % (label), classes=["%s" % (file_type)])
-        header_caption = [pf.Str("Listing:"), pf.Space(), pf.Str("%s" % (file_title))]
-        # pf.debug(header_caption)
+            # header_caption = pf.Str("Listing: %s %s" % (file_title, anchor))
+            header_block = pf.CodeBlock("", identifier="lst:%s" % (label), classes=["%s" % (file_type)])
+            header_caption = [pf.Str("Listing:"), pf.Space(), pf.Str("%s" % (file_title))]
+            # pf.debug(header_caption)
 
-        # header = pf.Para(*header_block)
-        read = pf.CodeBlock(raw, classes=types, attributes={"numbers": "left"})
+            # header = pf.Para(*header_block)
+            read = pf.CodeBlock(raw, classes=types, attributes={"numbers": "left"})
 
-        cell = pf.TableCell(pf.Para(*header_caption), header_block)
-        # pf.debug(cell)
-        row = [pf.TableRow(cell)]
-        # pf.debug(row)
-        table = pf.Table(*row, alignment=['AlignDefault'], width=[1])
+            cell = pf.TableCell(pf.Para(*header_caption), header_block)
+            # pf.debug(cell)
+            row = [pf.TableRow(cell)]
+            # pf.debug(row)
+            table = pf.Table(*row, alignment=['AlignDefault'], width=[1])
 
-        pf.debug("listingtable of ", fn)
-        return [table, read]
+            pf.debug("listingtable of ", fn)
+            return [table, read]
 
 
 def main(doc=None):
-    return pf.run_filter(pf.yaml_filter, tag='listingtable', function=fenced_action,
+    lt = ListingTable()
+    return pf.run_filter(pf.yaml_filter, tag='listingtable', function=lt.listingtable,
                          doc=doc)
 
 
