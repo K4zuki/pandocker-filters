e9346a12dfc082d3561307535c8de43e0c613966
K4zuki <k.yamamoto.08136891@gmail.com>
2017-08-22 02:51:25 +0900

    update BitField filter and pointed in Makefile.in
    - filter gets options and prints as debug message
    - "input" is mandatory option otherwise AssertionError occurs

--- BitField.py
+++ BitField.py
@@ -13,8 +13,53 @@ panflute
 applies MIT License (c) K4ZUKI(k.yamamoto.08136891@gmail.com)
 """
 
+import panflute as pf
+
 
 class BitField(object):
 
     def __init__(self):
-        pass
+        self.bitfield = pf.shell("which bitfield").strip()
+
+    def generate(self, options, data, element, doc):
+        # pf.debug("generate()")
+        source = options.get('input')
+        assert source is not None, "mandatory option 'input' is not set"
+        vspace = str(options.get('vspace', 80))
+        hspace = str(options.get('hspace', 640))
+        lanes = str(options.get('lanes', 2))
+        bits = str(options.get('bits', 16))
+        fontfamily = '"' + options.get('fontfamily', '"source code pro"') + '"'
+        fontsize = str(options.get('fontsize', 16))
+        fontweight = options.get('fontweight', "normal")
+
+        # pf.debug(bitfield)
+        # pf.debug(source)
+        # pf.debug(vspace)
+        # pf.debug(hspace)
+        # pf.debug(lanes)
+        # pf.debug(bits)
+        # pf.debug(fontfamily)
+        # pf.debug(fontsize)
+        # pf.debug(fontweight)
+        pf.debug(" ".join([self.bitfield,
+                           "--input", source,
+                           "--vspace", vspace,
+                           "--hspace", hspace,
+                           "--lanes", lanes,
+                           "--bits", bits,
+                           "--fontfamily", fontfamily,
+                           "--fontsize", fontsize,
+                           "--fontweight", fontweight
+                           ]))
+        return
+
+
+def main(doc=None):
+    bf = BitField()
+    return pf.run_filter(pf.yaml_filter, tag='bitfield', function=bf.generate,
+                         doc=doc)
+
+
+if __name__ == "__main__":
+    main()
