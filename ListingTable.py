e5c4d4090c16e8f8deb6fe88c5208ce23e1d34fd
tklab <tklab@tkservgitlt01.diasemi.com>
2017-08-22 17:36:21 +0900

    Revert "Update ListingTable.py"
    This reverts commit fae95e2befd719ca0d79b45a55ee3401851a9c3d.

--- ListingTable.py
+++ ListingTable.py
@@ -1,151 +1,137 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-""" pandoc-bitfield
-Yet another pandoc filter which plays with bitfield interpreter
-the filter finds out code block of "bitfield" class,
-then throws given code or file to bitfield,
-saves generated image in specified directory.
-the codeblock will be replaced by an image link
-pandoc
-bitfield
-panflute
+"""
+Panflute filter to parse Any text in fenced YAML code blocks
+http://scorreia.com/software/panflute/guide.html#yaml-code-blocks
 
-applies MIT License (c) K4ZUKI(k.yamamoto.08136891@gmail.com)
+block shape looks like this:
+
+```listingtable
+source: <relative path to file to include>
+class: source file type
+tex: True if rendering in tex mode; exclusive with docx
+docx: True if rendering in docx mode; exclusive with tex
+---
+```
+
 """
 
-import os
+import io
+import csv
 import panflute as pf
-import subprocess
+import codecs
+import os
+
+"""
+    _type = type
+
+    _label = _basename.lower().replace(".", "_")
+    _label = _label.replace("/", "_")
+    anchor = "" if not isDocx else 'TC "[@lst:%s] %s" `\l` 6\n\n' % (_label, _basename)
+
+    _file_title = _basename if not isTex else _basename.replace("_", "\\\\\\_")
+
+    _list = ["Listing: %s %s" % (_file_title, anchor),
+             "```{#lst:%s %s}" % (_label, _type),
+             "```"]
+
++--------------------------------------------------------------------------------+
+|Listing: table.csv                                                              |
+|```{#lst:table_csv .csv}                                                        |
+|```                                                                             |
++--------------------------------------------------------------------------------+
+
+```{.csv .numberLines numbers="left"}
+this,is,a table,"multi\
+line\
+title"
+to,show,an,example
+of,table,markdown,"importer\
+of\
+multiline"
+```
+
+Table(TableRow(
+TableCell(Para(Str(Listing: ) Space Str(table.csv))
+CodeBlock(
+identifier='lst:table_csv', classes=['csv'])))
+alignment=['AlignDefault'], width=[1], rows=1, cols=1)
+"""
 
 
-class BitField(object):
+class ListingTable(object):
 
     def __init__(self):
-        self.bitfield = pf.shell("which bitfield").decode('utf-8').strip()
-        # self.bitfield = "./bitfield/bin/bitfield.js"
         self.counter = 0
-        self.unix = True if (os.name) != "nt" else False
-        self.svg2pdf = None
-        self.svg2png = None
-        self.svg2eps = None
-        if self.unix:
-            self.svg2pdf = pf.shell("which rsvg-convert").decode('utf-8').strip()
-            self.svg2png = self.svg2pdf
-            self.svg2eps = self.svg2eps
-        else:
-            self.svg2pdf = "'" + str(pf.shell("which svg2pdf").decode('utf-8').strip()) + "'"
-            self.svg2png = "'" + str(pf.shell("which svg2png").decode('utf-8').strip()) + "'"
-            pf.debug("non-UNIX OS!")
-        self.defaultdir_to = "svg"
 
-    def generate(self, options, data, element, doc):
-        # pf.debug("generate()")
-        source = options.get('input')
-        vspace = str(options.get('vspace', 80))
-        hspace = str(options.get('hspace', 640))
-        lanes = str(options.get('lanes', 2))
-        bits = str(options.get('bits', 16))
-        fontfamily = '"' + options.get('fontfamily', 'source code pro') + '"'
-        fontsize = str(options.get('fontsize', 16))
-        fontweight = options.get('fontweight', "normal")
-        caption = options.get('caption')
-        dir_to = options.get('directory', self.defaultdir_to)
-        toPNG = options.get('png', True)
-        toEPS = options.get('eps', False) if self.unix else False
-        toPDF = True if doc.format in ["latex"] else options.get('pdf', False)
-
-        # options.get('pdf', False)
-        # toPDF = options.get('pdf', False)
-        assert source is not None, "mandatory option 'input' is not set"
-        assert os.path.exists("./" + source) == 1, "input file does not exist"
-        assert isinstance(toPNG, bool), "option png is boolean"
-        assert isinstance(toPDF, bool), "option pdf is boolean"
-        assert isinstance(toEPS, bool), "option eps is boolean"
-
-        # pf.debug(isinstance(toPNG, bool))
-        pf.debug(toPDF)
-        pf.debug(doc.format)
-
-        # pf.debug(bitfield)
-        # pf.debug(source)
-        # pf.debug(vspace)
-        # pf.debug(hspace)
-        # pf.debug(lanes)
-        # pf.debug(bits)
-        # pf.debug(fontfamily)
-        # pf.debug(fontsize)
-        # pf.debug(fontweight)
-        if os.path.exists("./" + dir_to) != 1:
-            os.mkdir("./" + dir_to)
-
-        _basename = "/".join([".",
-                              dir_to,
-                              str(self.counter)])
-        _svg = ".".join([_basename, "svg"])
-        # svg2pdf file.svg --output out.pdf
-        # optional arguments:
-        #   -h, --help            show this help message and exit
-        #   -v, --version         Print version number and exit.
-        #   -o PATH_PAT, --output PATH_PAT
-        # $(RSVG) $<.svg --format=png --output=$@
-        # $(RSVG) $<.svg --output $@
-
-        toSVG = [self.bitfield,
-                 "--input", source,
-                 "--vspace", vspace,
-                 "--hspace", hspace,
-                 "--lanes", lanes,
-                 "--bits", bits,
-                 "--fontfamily", fontfamily,
-                 "--fontsize", fontsize,
-                 "--fontweight", fontweight,
-                 ">", _svg
-                 ]
-        pf.debug(" ".join(toSVG))
-        pf.shell(" ".join(toSVG))
-        # subprocess.call(toSVG)
-
-        if(toPDF):
-            output = [self.svg2pdf, _svg]
-            if self.unix:
-                output.append("--format=pdf")
-            output.append("--output")
-            output.append(".".join([_basename, "pdf"]))
-            pf.debug(" ".join(output))
-            pf.shell(" ".join(output))
-            subprocess.call(output)
-
-        if(toPNG):
-            output = [self.svg2png, _svg]
-            if self.unix:
-                output.append("--format=png")
-            output.append("--output")
-            output.append(".".join([str(_basename), "png"]))
-            pf.debug(" ".join(output))
-            pf.shell(" ".join(output))
-            # subprocess.call(output)
-
-        if(toEPS):
-            output = [self.svg2pdf,
-                      _svg,
-                      "--format=eps"
-                      "--output",
-                      ".".join([_basename, "eps"])
-                      ]
-            pf.debug(" ".join(output))
-            pf.shell(" ".join(output))
-            # subprocess.call(output)
-
-        # [pf.debug(command) for command in [_svg2pdf, _svg2png, _svg2eps]]
-        # [pf.shell(command) for command in [_svg2pdf, _svg2png, _svg2eps]]
+    def listingtable(self, options, data, element, doc):
         self.counter += 1
-        return None
+        # We'll only run this for CodeBlock elements of class 'listingtable'
+
+        # pf.debug(doc.get_metadata('include', 'no hoge'))
+        # pf.debug(element.parent)
+        # pf.debug(element.index)
+        # pf.debug(element.parent.content)
+
+        # pf.debug(element.parent)
+        # pf.debug(element.prev)
+        # pf.debug(element)
+        # pf.debug(element.next)
+
+        fn = options.get('source')
+        basename = os.path.basename(fn)
+        isTex = options.get('tex', False)
+        isDocx = options.get('isDocx', False)
+        file_type = options.get('class', 'plain')
+        types = [file_type, 'numberLines']
+        # pf.debug(file_type, types)
+        for doctype in [isTex, isDocx]:
+            if not isinstance(doctype, bool):
+                if isinstance(doctype, str):
+                    doctype = doctype.upper()
+                    if doctype in ['TRUE', 'YES']:
+                        doctype = True
+                    else:
+                        doctype = False
+                else:
+                    doctype = False
+
+        if isTex and isDocx:
+            pass
+        else:
+            with codecs.open(fn, 'r', 'utf-8') as f:
+                # pf.debug("codecs.open()")
+                raw = f.read()
+            # pf.debug(raw)
+
+            label = basename.lower().replace(".", "_").replace("/", "_") + str(self.counter)
+            anchor = "" if not isDocx else 'TC "[@lst:%s] %s" `\l` 6\n\n' % (label, basename)
+            file_title = basename if not isTex else basename.replace("_", "\\\\\\_")
+            # pf.debug(label, anchor, file_title)
+
+            # header_caption = pf.Str("Listing: %s %s" % (file_title, anchor))
+            header_block = pf.CodeBlock("", identifier="lst:%s" % (label), classes=["%s" % (file_type)])
+            header_caption = [pf.Str("Listing:"), pf.Space(), pf.Str("%s" % (file_title))]
+            # pf.debug(header_caption)
+
+            # header = pf.Para(*header_block)
+            read = pf.CodeBlock(raw, classes=types, attributes={"numbers": "left"})
+
+            cell = pf.TableCell(pf.Para(*header_caption), header_block)
+            # pf.debug(cell)
+            row = [pf.TableRow(cell)]
+            # pf.debug(row)
+            table = pf.Table(*row, alignment=['AlignDefault'], width=[1])
+
+            pf.debug("listingtable of ", fn)
+            return [table, read]
 
 
 def main(doc=None):
-    bf = BitField()
-    return pf.run_filter(pf.yaml_filter, tag='bitfield', function=bf.generate, doc=doc)
+    lt = ListingTable()
+    return pf.run_filter(pf.yaml_filter, tag='listingtable', function=lt.listingtable,
+                         doc=doc)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
