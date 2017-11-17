319304221a9a2a47015699b1450608b1177534d5
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2017-11-18 02:46:42 +0900

    listingtable-inline.py uses Link instead of Image
    - this avoids build failure(but may miss error)
    - only works with non-inline case otherwise ignored(and makes link to
    source code)

--- listingtable-inline.py
+++ listingtable-inline.py
@@ -16,52 +16,51 @@ class inline_listingtable(ListingTable):
         super().__init__()
 
     def action(self, elem, doc):
-        ret = []
-        if isinstance(elem, pf.Para) and hasattr(elem.content, "pf.Image"):
-            # if isinstance(elem, pf.Image):
+        self.doc = doc
+        if isinstance(elem, (pf.Para)) and len(elem.content) == 1:
             for subelem in elem.content:
-                # subelem = elem.content[0]
-                # pf.debug(isinstance(elem.content[0], pf.Image))
-                if isinstance(subelem, pf.Image) and 'listingtable' in subelem.classes:
-                    index = elem.content.index(subelem)
-                    pf.debug(index)
-                    self.doc = doc
-                    self.counter -= 1
-                    # pf.debug("inline_listingtable.action()")
-                    # pf.debug(subelem)
-                    fn = subelem.url
-                    options = subelem.attributes
-                    idn = subelem.identifier
-                    caption = subelem.content
-                    # pf.debug(fn)
-                    # pf.debug(options)
-                    # pf.debug(idn)
-                    # pf.debug(len(caption))
-                    basename = os.path.basename(fn)
-                    label = basename.lower().replace(".", "_").replace("/", "_") + str(self.counter)
-                    idn = idn if idn else "lst:{label:s}".format(label=label)
+                if isinstance(subelem, pf.Link) and 'listingtable' in subelem.classes:
+                    elem = self.listingtable(subelem)
+        return elem
 
-                    file_type = options.get('type', 'text')
-                    # pf.debug(file_type)
+    def listingtable(self, elem):
+        self.counter -= 1
+        # pf.debug("inline_listingtable.action()")
+        # pf.debug(subelem)
+        fn = elem.url
+        options = elem.attributes
+        idn = elem.identifier
+        caption = elem.content
+        # pf.debug(fn)
+        # pf.debug(options)
+        # pf.debug(idn)
+        # pf.debug(len(caption))
+        basename = os.path.basename(fn)
+        label = basename.lower().replace(".", "_").replace("/", "_") + str(self.counter)
+        idn = idn if idn else "lst:{label:s}".format(label=label)
 
-                    types = [file_type, 'numberLines']
+        file_type = options.get('type', 'text')
+        # pf.debug(file_type)
 
-                    file_title = basename
-                    if self.doc.format in ["latex"]:
-                        file_title = basename.replace("_", "\textunderscore")
-                    caption = [pf.Str("%s" % (file_title))] if not len(caption) else caption
-                    header_caption = pf.Para(pf.Str("Listing:"), pf.Space(), *caption)
+        types = [file_type, 'numberLines']
 
-                    with open(fn, 'r', encoding='utf-8') as f:
-                        raw = f.read()
-                        # data = self.validatejson(data)
-                    attr = {"numbers": "left"}
-                    read = pf.CodeBlock(raw, classes=types, identifier=idn, attributes=attr)
+        file_title = basename
+        if self.doc.format in ["latex"]:
+            file_title = basename.replace("_", "\textunderscore")
+            temp_caption = [pf.Str("%s" % (file_title))]
+        caption = temp_caption if not len(caption) else caption
 
-                    pf.debug("inline listingtable of", fn)
-                    ret.append(header_caption)
-                    ret.append(read)
-            return ret
+        with open(fn, 'r', encoding='utf-8') as f:
+            raw = f.read()
+            # data = self.validatejson(data)
+        attr = {"numbers": "left"}
+        read = pf.CodeBlock(raw, classes=types, identifier=idn, attributes=attr)
+
+        pf.debug("[inline] inline listingtable of", fn)
+
+        ret = [pf.Para(pf.Str("Listing:"), pf.Space(), *caption), read]
+        # pf.debug(ret)
+        return ret
 
 
 def main(doc=None):
