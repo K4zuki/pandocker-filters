b3c09a2bcea9965c24fbd49732bfbfc5ea18ad6d
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2017-11-19 16:19:40 +0900

    update ListingTable filters
    - merged common work into listingtable()
    - action() collects certain values and then calls listingtable()

--- ListingTable.py
+++ ListingTable.py
@@ -64,73 +64,47 @@ class ListingTable(object):
     def __init__(self):
         self.counter = 0
 
-    def listingtable(self, options, data, element, doc):
+    def action(self, options, data, element, doc):
+        self.doc = doc
         self.counter += 1
         # We'll only run this for CodeBlock elements of class 'listingtable'
 
-        # pf.debug(doc.get_metadata('include', 'no hoge'))
-        # pf.debug(element.parent)
-        # pf.debug(element.index)
-        # pf.debug(element.parent.content)
-
-        # pf.debug(element.parent)
-        # pf.debug(element.prev)
-        # pf.debug(element)
-        # pf.debug(element.next)
-
         fn = options.get('source')
-        basename = os.path.basename(fn)
-        isTex = options.get('tex', False)
-        isDocx = options.get('isDocx', False)
-        file_type = options.get('class', 'plain')
+        idn = element.identifier
+        caption = options.get('caption', "")
+        ret = self.listingtable(filename=fn, idn=idn, caption=caption, options=options)
+        return ret
+
+    def listingtable(self, filename, idn, caption, options):
+        basename = os.path.basename(filename)
+        file_type = options.get('type', 'plain')
         types = [file_type, 'numberLines']
-        # pf.debug(file_type, types)
-        for doctype in [isTex, isDocx]:
-            if not isinstance(doctype, bool):
-                if isinstance(doctype, str):
-                    doctype = doctype.upper()
-                    if doctype in ['TRUE', 'YES']:
-                        doctype = True
-                    else:
-                        doctype = False
-                else:
-                    doctype = False
+        attr = {"numbers": "left"}
 
-        if isTex and isDocx:
-            pass
+        if self.doc.format in ["latex"]:
+            file_title = basename.replace("_", "\textunderscore")
         else:
-            with codecs.open(fn, 'r', 'utf-8') as f:
-                # pf.debug("codecs.open()")
-                raw = f.read()
-            # pf.debug(raw)
+            file_title = basename
 
-            label = basename.lower().replace(".", "_").replace("/", "_") + str(self.counter)
-            anchor = "" if not isDocx else 'TC "[@lst:%s] %s" `\l` 6\n\n' % (label, basename)
-            file_title = basename if not isTex else basename.replace("_", "\\\\\\_")
-            # pf.debug(label, anchor, file_title)
+        temp_caption = [pf.Str("%s" % (file_title))]
+        caption = temp_caption if not len(caption) else caption
 
-            # header_caption = pf.Str("Listing: %s %s" % (file_title, anchor))
-            header_block = pf.CodeBlock("", identifier="lst:%s" % (label), classes=["%s" % (file_type)])
-            header_caption = [pf.Str("Listing:"), pf.Space(), pf.Str("%s" % (file_title))]
-            # pf.debug(header_caption)
+        with open(filename, 'r', encoding='utf-8') as f:
+            raw = f.read()
 
-            # header = pf.Para(*header_block)
-            read = pf.CodeBlock(raw, classes=types, attributes={"numbers": "left"})
+        label = basename.lower().replace(".", "_").replace("/", "_") + str(self.counter)
+        idn = idn if idn else "lst:{label:s}".format(label=label)
 
-            cell = pf.TableCell(pf.Para(*header_caption), header_block)
-            # pf.debug(cell)
-            row = [pf.TableRow(cell)]
-            # pf.debug(row)
-            table = pf.Table(*row, alignment=['AlignDefault'], width=[1])
+        read = pf.CodeBlock(raw, classes=types, identifier=idn, attributes=attr)
 
-            pf.debug("listingtable of", fn)
-            return [table, read]
+        pf.debug("listingtable of", filename)
+        ret = [pf.Para(pf.Str("Listing:"), pf.Space(), *caption), read]
+        return ret
 
 
 def main(doc=None):
     lt = ListingTable()
-    return pf.run_filter(pf.yaml_filter, tag='listingtable', function=lt.listingtable,
-                         doc=doc)
+    return pf.run_filter(pf.yaml_filter, tag='listingtable', function=lt.action, doc=doc)
 
 
 if __name__ == '__main__':
--- listingtable-inline.py
+++ listingtable-inline.py
@@ -20,48 +20,15 @@ class inline_listingtable(ListingTable):
         if isinstance(elem, (pf.Para)) and len(elem.content) == 1:
             for subelem in elem.content:
                 if isinstance(subelem, pf.Link) and 'listingtable' in subelem.classes:
-                    elem = self.listingtable(subelem)
+                    self.counter -= 1
+                    fn = subelem.url
+                    options = subelem.attributes
+                    idn = subelem.identifier
+                    caption = subelem.content
+                    elem = self.listingtable(filename=fn, idn=idn, caption=caption, options=options)
+                    # return ret
+                    # elem = self.listingtable(subelem)
         return elem
-
-    def listingtable(self, elem):
-        self.counter -= 1
-        # pf.debug("inline_listingtable.action()")
-        # pf.debug(subelem)
-        fn = elem.url
-        options = elem.attributes
-        idn = elem.identifier
-        caption = elem.content
-        # pf.debug(fn)
-        # pf.debug(options)
-        # pf.debug(idn)
-        # pf.debug(len(caption))
-        basename = os.path.basename(fn)
-        label = basename.lower().replace(".", "_").replace("/", "_") + str(self.counter)
-        idn = idn if idn else "lst:{label:s}".format(label=label)
-
-        file_type = options.get('type', 'text')
-        # pf.debug(file_type)
-
-        types = [file_type, 'numberLines']
-
-        if self.doc.format in ["latex"]:
-            file_title = basename.replace("_", "\textunderscore")
-        else:
-            file_title = basename
-        temp_caption = [pf.Str("%s" % (file_title))]
-        caption = temp_caption if not len(caption) else caption
-
-        with open(fn, 'r', encoding='utf-8') as f:
-            raw = f.read()
-            # data = self.validatejson(data)
-        attr = {"numbers": "left"}
-        read = pf.CodeBlock(raw, classes=types, identifier=idn, attributes=attr)
-
-        pf.debug("[inline] inline listingtable of", fn)
-
-        ret = [pf.Para(pf.Str("Listing:"), pf.Space(), *caption), read]
-        # pf.debug(ret)
-        return ret
 
 
 def main(doc=None):
