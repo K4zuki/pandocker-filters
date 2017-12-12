9d9a3be6f6aebb261da9687fd4693bf21ca7da82
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2017-12-13 00:57:36 +0900

    update setup files

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
+# Always prefer setuptools over distutils
+from setuptools import find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
 from distutils.core import setup
+
+requires = ["panflute"]
 
 setup(
     name="pandoc_pandocker_filters",
-    packages=["pandoc-pandocker-filters"],
-    version="0.0.1",  # Ideally should be same as your GitHub release tag varsion
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    version="0.0.2",  # Ideally should be same as your GitHub release tag varsion
     description="Yet another pandoc filter group",
-    author="K4zuki",
-    author_email="K4zuki@github.com",
+    author="k4zuki",
+    author_email="k.yamamoto.08136891@gmail.com",
     url="https://github.com/K4zuki/pandocker-filters",
-    download_url="https://github.com/K4zuki/pandocker-filters/archive/0.0.1.tar.gz",
+    install_requires=requires,
     keywords=["pandoc", "markdown"],
     classifiers=[],
 )
