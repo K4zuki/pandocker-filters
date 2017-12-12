5e20cbd8c6b1e78999ba2147b0979c33b0bf2fdd
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2017-12-12 23:45:10 +0900

    pypi package preparation

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="pandoc_pandocker_filters",
+    packages=["pandoc-pandocker-filters"],
+    version="0.0.1",  # Ideally should be same as your GitHub release tag varsion
+    description="Yet another pandoc filter group",
+    author="K4zuki",
+    author_email="K4zuki@github.com",
+    url="github package source url",
+    download_url="download link you saved",
+    keywords=["pandoc", "markdown"],
+    classifiers=[],
+)
