aea09149fbcb82c784515053eb6c13172fb2e1a8
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2017-12-13 01:32:39 +0900

    add console scripts targets
    - single quote to double quote
    - update description
    - update version

--- setup.py
+++ setup.py
@@ -9,13 +9,27 @@ requires = ["panflute"]
 
 setup(
     name="pandoc_pandocker_filters",
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    version="0.0.2",  # Ideally should be same as your GitHub release tag varsion
-    description="Yet another pandoc filter group",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    version="0.0.4",  # Ideally should be same as your GitHub release tag varsion
+    description="Yet another set of pandoc filters",
     author="k4zuki",
     author_email="k.yamamoto.08136891@gmail.com",
     url="https://github.com/K4zuki/pandocker-filters",
     install_requires=requires,
     keywords=["pandoc", "markdown"],
     classifiers=[],
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        "console_scripts": [
+            "pandocker-bitfield = pandoc_pandocker_filters.BitField:main",
+            "pandocker-bitfield-inline = pandoc_pandocker_filters.bitfield_inline:main"
+            "pandocker-listingtable = pandoc_pandocker_filters.ListingTable:main"
+            "pandocker-listingtable-inline = pandoc_pandocker_filters.listingtable_inline:main"
+            "pandocker-wavedrom-inline = pandoc_pandocker_filters.wavedrom_inline:main"
+            "pandocker-rotateimage = pandoc_pandocker_filters.RotteImage:main"
+            "pandocker-rotateimage-inline = pandoc_pandocker_filters.rotateimage-inline:main"
+        ],
+    },
 )
