9c6827379c4248124e81b75f1daec8c2a996b372
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2018-01-13 20:43:58 +0900

    update setup.py
    - add dependencies
    - add aafigure(-inline) in entry_points{}

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ from codecs import open
 from os import path
 from distutils.core import setup
 
-requires = ["panflute"]
+requires = ["panflute", "aafigure", "svgutils", "reportlab"]
 
+VERSION = "0.0.4"
 setup(
     name="pandoc_pandocker_filters",
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
-    version="0.0.4",  # Ideally should be same as your GitHub release tag varsion
+    version=VERSION,  # Ideally should be same as your GitHub release tag varsion
     description="Yet another set of pandoc filters",
     author="k4zuki",
     author_email="k.yamamoto.08136891@gmail.com",
@@ -30,6 +31,8 @@ setup(
             "pandocker-wavedrom-inline = pandoc_pandocker_filters.wavedrom_inline:main",
             "pandocker-rotateimage = pandoc_pandocker_filters.RotateImage:main",
             "pandocker-rotateimage-inline = pandoc_pandocker_filters.rotateimage_inline:main",
+            # "pandocker-aafigure = pandoc_pandocker_filters.AAFigure:main",
+            # "pandocker-aafigure-inline = pandoc_pandocker_filters.aafigure_inline:main",
         ],
     },
 )
