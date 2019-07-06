5b030754c994eb11daefd1b3a5e8ce77e07ee316
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2019-07-06 23:18:30 +0900

    drop bitfield_inline from pandocker-filters

--- pandoc_pandocker_filters/__init__.py
+++ pandoc_pandocker_filters/__init__.py
@@ -1,6 +1,6 @@
 import panflute as pf
 from . import aafigure_inline
-from . import bitfield_inline
+# from . import bitfield_inline
 from . import listingtable_inline
 from . import rotateimage_inline
 from . import wavedrom_inline
@@ -9,12 +9,14 @@ from . import pantable_inline
 
 def main(doc=None):
     aaf = aafigure_inline.inline_aafigure()
-    bf = bitfield_inline.inline_bitfield()
+    # bf = bitfield_inline.inline_bitfield()
     lt = listingtable_inline.inline_listingtable()
     roi = rotateimage_inline.rotateimage_inline()
     wd = wavedrom_inline.wavedrom_inline()
     pt = pantable_inline.pantable_inline()
-    pf.run_filters([aaf.action, bf.action, lt.action, wd.action, pt.action, roi.action], doc=doc)
+    pf.run_filters([aaf.action,
+                    # bf.action,
+                    lt.action, wd.action, pt.action, roi.action], doc=doc)
     return doc
 
 
