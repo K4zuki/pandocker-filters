aed269905582d53c177b9e8003eb681adc5f244c
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2019-07-06 13:49:51 +0900

    un-limit wavedrompy version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requires = ["panflute>=1.10.3",
             "svgutils>=0.2.0",
             "svglib",
             "aafigure",
-            "wavedrom==1.8.0",
+            "wavedrom",
             "pantable",
             ]
 
